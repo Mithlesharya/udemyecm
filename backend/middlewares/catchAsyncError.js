04aaaff8f9cc6fd6e4574c7c895acc9e4c570f04
themscool <design.themscool@gmail.com>
2023-01-11 12:03:21 +0530

    adding comments

--- backend/middlewares/catchAsyncError.js
+++ backend/middlewares/catchAsyncError.js
@@ -1,4 +1,4 @@
-
+// creating global catch async error for api
   export const catchAsyncError = func => (req, res, next) =>{
     Promise.resolve(func(req, res, next))
     .catch(next)
--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -1,7 +1,9 @@
 export const errorsMiddlewares = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || 'enter server error';
+    
 
+    //development error
     if (process.env.NODE_ENV === 'DEVELOPMENT') {
         res.status(err.statusCode).json({
             success: false,
@@ -10,6 +12,8 @@ export const errorsMiddlewares = (err, req, res, next) => {
             stack: err.stack
         })
     }
+
+    //production error 
     if (process.env.NODE_ENV === 'PRODUCTION') {
         let error = { ...err };
         error.message = err.message
