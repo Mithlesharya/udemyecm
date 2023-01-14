1b8b10aa9c97b58cf021489d4267f011c1a4ba1f
themscool <design.themscool@gmail.com>
2023-01-14 11:15:00 +0530

    Authorize User Roles and Permissions.

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -7,9 +7,9 @@ const router = express.Router();
 
 router.get('/products', getProducts);
 router.get('/products/:id', getSingleProduct);
-router.post('/products/new', isAuthenticatedUser, newProduct);
-router.put('/products/:id', isAuthenticatedUser, updateProduct);
-router.delete('/products/:id', isAuthenticatedUser, deleteProduct);
+router.post('/products/new', isAuthenticatedUser, newProduct); //isAuthenticatedUser prtect to create our prodcut, only create when user logined
+router.put('/products/:id', isAuthenticatedUser, updateProduct); //isAuthenticatedUser prtect to create our prodcut, only create when user logined
+router.delete('/products/:id', isAuthenticatedUser, deleteProduct); //isAuthenticatedUser prtect to create our prodcut, only create when user logined
 
 
 
